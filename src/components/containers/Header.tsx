4434fdff2d0510ce4c6940f16238b7493023304a
Yassine Jebli <yassine@mobilly.lv>
2020-09-26 21:29:16 +0300

    Create .eslintrc

--- src/components/containers/Header.tsx
+++ src/components/containers/Header.tsx
@@ -1,33 +1,40 @@
-import React from 'react';
-import styled from 'styled-components';
-import {useDispatch, useSelector} from "react-redux";
-import {filterTextItems} from "../../actions";
-import {State} from "../../reducers";
-export default function Header(){
-    const dispatch = useDispatch();
-    const searchText = useSelector<State, string>(state => state.searchText);
+import React from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { filterTextItems } from "../../actions";
+import { State } from "../../reducers";
+export default function Header() {
+  const dispatch = useDispatch();
+  const searchText = useSelector<State, string>((state) => state.searchText);
 
-
-    const onFilter = ({target:{value}}: React.ChangeEvent<HTMLInputElement>) => {
-        dispatch(filterTextItems(value))
-    }
-    return <Wrapper>
-        <SearchInput value={searchText} onChange={onFilter} type="search" placeholder="Search" />
-        <VerticalLine />
-        <AddButton>+</AddButton>
+  const onFilter = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(filterTextItems(value));
+  };
+  return (
+    <Wrapper>
+      <SearchInput
+        value={searchText}
+        onChange={onFilter}
+        type="search"
+        placeholder="Search"
+      />
+      <VerticalLine />
+      <AddButton>+</AddButton>
     </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
   padding: 6px;
   display: flex;
-  
-`
+`;
 
 const SearchInput = styled.input`
   padding: 8px;
   width: 100%;
-`
+`;
 
 const VerticalLine = styled.div`
   border-left: 1px solid #000;
@@ -41,11 +48,10 @@ const AddButton = styled.button`
   border: none;
   border-radius: 50%;
   cursor: pointer;
-  &:hover{
+  &:hover {
     background-color: rgba(137, 176, 240, 0.1);
   }
   &:focus {
-    outline:0;
+    outline: 0;
   }
- 
-`
+`;
